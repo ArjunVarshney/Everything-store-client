345496baea84d38671d40867e3e0f4f4b0d43481
Arjun Varshney <varshneyarjun49@gmail.com>
2023-10-18 19:20:12 +0530

    changed database

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,18 +7,20 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const Home = async () => {
-  const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0a5287a9-ffb5-495b-b25c-9c535d954cdd");
-  return (
-    <Container>
-      <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products} />
-        </div>
-      </div>
-    </Container>
-  );
+   const products = await getProducts({ isFeatured: true });
+   const billboard = await getBillboard(
+      process.env.MAIN_BILLBOARD_ID || "fe701839-456d-4e15-a281-720c7066a6a6"
+   );
+   return (
+      <Container>
+         <div className="space-y-10 pb-10">
+            <Billboard data={billboard} />
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+               <ProductList title="Featured Products" items={products} />
+            </div>
+         </div>
+      </Container>
+   );
 };
 
 export default Home;
